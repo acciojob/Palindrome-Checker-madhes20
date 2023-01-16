cb8a31302275ed5d610aa8a516798ce0ee048b0f
madhes20 <118300068+madhes20@users.noreply.github.com>
2023-01-16 15:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+	let len = str.length;
+	let str_uppername = str.toUpperCase();
+    var j=len-1;
+    for(var i=0; i<len/2; i++){
+        if(str_uppername[i] === str_uppername[j]){
+            j--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
